d1446b423909d690a4732d6439013cea8c20bd71
tthien <ng.t.thien01@gmail.com>
2025-05-10 23:36:47 +0700

    Fix

--- app/music/components/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-// This is an intentionally empty component file
-// It exists only to satisfy Next.js's file routing requirements
-// The actual content will be loaded dynamically via the root page
-
-export default function EmptyComponent() {
-  return null;
-}
